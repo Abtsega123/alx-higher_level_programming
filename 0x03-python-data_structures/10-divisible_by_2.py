0ba5f06d3fa38bce099dbc47542579f82b4d9403
Abtsega123 <andyboringo1@gmail.com>
2022-06-07 01:47:09 +0530

    commited

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return
-    new_list = [n % 2 == 0 for n in my_list]
-
-    return 
+    boolist = my_list[:]
+    for count, i in enumerate(my_list):
+        if i % 2 == 0:
+            boolist[count] = True
+        else:
+            boolist[count] = False
+    return(boolist)
