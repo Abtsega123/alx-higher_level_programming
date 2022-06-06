1743bea578cd47aa8abb54e3d423999fca20fbb1
Abtsega123 <andyboringo1@gmail.com>
2022-06-07 01:45:41 +0530

    commited

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,21 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * is_palindrome - check if a linked list is a palindrome
- * @head: pointer to head of list
- * Return: 0 or 1
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int is_palindrome(listint_t **head)
+int main(void)
 {
-    const listint_t *current;
-    const listint_t *tail;
-    unsigned int n = 0;
+	listint_t *head;
 
-    //Get length
-    //Use a function to get a node on a specific index
-    //check the first to the last and, second to second last ....
-    //return false if one check was False
-    //return true at the end (if all passed the checks)
-
-    return (n);
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
